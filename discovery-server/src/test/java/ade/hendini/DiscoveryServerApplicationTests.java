78aac43c51c9b310f6b202d63c58cc5474e07927
adehendini <adehendini@gmail.com>
2022-10-24 09:23:10 +0700

    discovery server

---
+++ discovery-server/src/test/java/ade/hendini/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ade.hendini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
